d13a5ca9fb51ed54db9f5eee920cd2d0266a4f78
sisaurora <sisaurora@gmail.com>
2015-09-07 21:47:28 -0300

    melhorias no css

--- Boot.js
+++
@@ -1,3 +0,0 @@
-document.write("https://github.jspm.io/jmcriffey/bower-traceur-runtime@0.0.87/traceur-runtime.js'></script>");
-document.write("https://jspm.io/system@0.16.js'></script>");
-document.write("https://code.angularjs.org/2.0.0-alpha.28/angular2.dev.js'></script>");
